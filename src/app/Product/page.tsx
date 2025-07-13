934556c4eda862df30f03a22ecf2800df3705c8d
Abdullah mohamed Elkohly <116310489+abdulaah345@users.noreply.github.com>
2025-07-13 21:01:04 +0300

    first commit

---
+++ src/app/Product/page.tsx
@@ -0,0 +1,51 @@
+"use client";
+import React, { useContext, useState } from "react";
+import toast, { Toaster } from "react-hot-toast";
+import { Shoppingcartcontext } from "../context/page";
+
+type producttype = {
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+  imageUrl: string;
+  categoryId: number;
+  categoryName: string;
+};
+
+const Product = ({ product }: { product: producttype }) => {
+  const { setcartitems } = useContext(Shoppingcartcontext);
+
+  const handelClick = (pro: producttype) => {
+    toast("Added to Cart");
+    setcartitems((prev) => {
+      return [...prev, pro];
+    });
+  };
+  return (
+    <div className="bg-white rounded-lg shadow-md overflow-hidden flex flex-col justify-between">
+      <img
+        src={product.imageUrl}
+        alt={product.name}
+        className="w-full h-56 object-cover"
+      />
+      <div className="p-4 flex-1">
+        <h2 className="text-xl font-semibold mb-2">{product.name}</h2>
+        <p className="text-gray-600 mb-2">{product.description}</p>
+        <p className="text-lg font-bold">{product.price} ج.م</p>
+      </div>
+      <div className=" mb-2 flex justify-center items-center  ">
+        <button
+          onClick={() => handelClick(product)}
+          style={{ backgroundColor: "#d49f68" }}
+          className="px-4 py-2  text-white rounded "
+        >
+          Add to Card
+          <Toaster />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Product;
